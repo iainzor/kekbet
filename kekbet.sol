c71dca6b13d1c8a6ca6d9a47aa02779090498d2e
iainzor <iainedminster@gmail.com>
2021-01-05 16:56:18 -0800

    Cleanup and error fixes

--- kekbet.sol
+++ kekbet.sol
@@ -2,26 +2,25 @@ pragma solidity ^0.4.24;
 
 import "https://github.com/smartcontractkit/chainlink/blob/develop/evm-contracts/src/v0.4/ChainlinkClient.sol";
 
-contract KEKbet {
-
- constructor() public {
-        setPublicChainlinkToken();
-        oracle = 0xA42FdFD2E1a7239B76D753803cbB7611004FE068; // oracle address
-        jobId = "6e5ea740bca64bf596288cef75707f51"; //job id
-        fee = 0.1 * 10 ** 18; // 0.1 LINK
-    }
-    struct Bets {
+contract KEKbet is ChainlinkClient {
+    struct Bet {
         address etherAddress;
         uint amount;
     }
-
-    Bets[] public voteA;
-    Bets[] public voteB;
-    uint public teamA = 0; // balance of all bets on teamA
-    uint public teamB = 0; // balance of all bets on teamB
+    
+    address oracle = 0xA42FdFD2E1a7239B76D753803cbB7611004FE068; // oracle address
+    bytes32 jobId = bytes32("6e5ea740bca64bf596288cef75707f51"); //job id
+    uint256 fee = 0.1 * 10 ** 18; // 0.1 LINK
     uint8 public house_earnings = 4; // percent
     uint public betLockTime = 0; // block
     uint public lastTransactionRec = 0; // block
+
+    Bet[] public voteA;
+    Bet[] public voteB;
+    
+    uint public teamA = 0; // balance of all bets on teamA
+    uint public teamB = 0; // balance of all bets on teamB
+    
     address public owner;
     address public coowner = 0x618A9Df7c2Df1567583EB03926472Ffd7FcE5423;
 
@@ -32,17 +31,23 @@ contract KEKbet {
 
     modifier onlyowner { if (msg.sender == owner) _; }
 
-    function KEKbets() {
+    constructor() public {
+        setPublicChainlinkToken();
+        
+    }
+    
+
+    function KEKbets() public {
         owner = msg.sender;
         lastTransactionRec = block.number;
     }
 
-    function() payable {
+    function() payable public {
         // if less than minBetAmount ETH or bet locked return money
         // If bet is locked for more than 28 days allow users to return all the money
         if (msg.value < minBetAmount ||
                         (block.number >= betLockTime && betLockTime != 0 && block.number < betLockTime + monthBlockCount)) {
-            require();
+            require(false);
         }
 
         uint amount;
@@ -80,25 +85,28 @@ contract KEKbet {
     }
 
     // no further ether will be accepted (fe match is now live)
-    function lockBet(uint blocknumber) onlyowner{
+    function lockBet(uint blocknumber) public onlyowner {
         betLockTime = blocknumber;
     }
     
- function getResults() public {
+    function getResults() public {
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfillResults.selector);
         sendChainlinkRequestTo(oracle, req, fee);
     }
-      function fulfillResults(bytes32 _requestId, uint256 _finalScore) public recordChainlinkFulfillment(_requestId) {
-        getResults = _finalScore;
+    
+    function fulfillResults(bytes32 _requestId, uint256 _finalScore) public recordChainlinkFulfillment(_requestId) {
+        // do something here
+        require(_finalScore > 0);
     }
     
     // init Finalize
-    function Finalize(uint winner) onlyowner {
-        var winPot = (winner == 0) ? teamA : teamB;
-        var losePot_ = (winner == 0) ? teamB : teamA;
+    function Finalize(uint winner) public onlyowner {
+        uint winPot = (winner == 0) ? teamA : teamB;
+        uint losePot_ = (winner == 0) ? teamB : teamA;
         uint losePot = losePot_ * (100-house_earnings) / 100; // substract housecut
         uint collectedFees = losePot_ * house_earnings / 100;
-        var winners = (winner == 0) ? voteA : voteB;
+        Bet[] storage winners = (winner == 0) ? voteA : voteB;
+        
         for(uint idx = 0; idx < winners.length; idx+=1){
             uint winAmount = winners[idx].amount + (winners[idx].amount * losePot / winPot);
             if(! winners[idx].etherAddress.send(winAmount)){ // If not successfull (invalid address) add to fee pool
@@ -118,7 +126,7 @@ contract KEKbet {
 
     // basically private (only called if last transaction was 4 weeks ago)
     // If a match is fixed or a party cheated, I will return all transactions manually.
-    function returnAll() onlyowner {
+    function returnAll() public onlyowner {
         for(uint idx = 0; idx < voteA.length; idx+=1){
             voteA[idx].etherAddress.transfer(voteA[idx].amount);
         }
@@ -128,7 +136,7 @@ contract KEKbet {
         clear();
     }
 
-    function clear() private{
+    function clear() private {
     	teamA = 0;
     	teamB = 0;
     	betLockTime = 0;
@@ -137,24 +145,21 @@ contract KEKbet {
     	delete voteB;
     }
 
-    function changeMinBetAmount(uint minBet) onlyowner {
+    function changeMinBetAmount(uint minBet) public onlyowner {
 	    minBetAmount = minBet;
     }
 
-    function changeMaxBetAmount(uint maxBet) onlyowner {
+    function changeMaxBetAmount(uint maxBet) public onlyowner {
 	    maxBetAmount = maxBet;
     }
 
-    function changeHouseEarnings(uint8 cut) onlyowner {
+    function changeHouseEarnings(uint8 cut) public onlyowner {
 	    // houseEarning boundaries
     	if(cut <= 20 && cut > 0)
     	    house_earnings = cut;
     }
 
-    function setOwner(address _owner) onlyowner {
+    function setOwner(address _owner) public onlyowner {
         owner = _owner;
     }
-
 }
-
-  
